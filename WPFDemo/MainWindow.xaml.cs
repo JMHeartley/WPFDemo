b380e8e70abe9dfea9ab7038978086a4e59861e9
Justin Heartley <heartleyjm@gmail.com>
2023-04-06 13:37:14 -0400

    FEAT: say hello
    * added a textbox that says "Hello there!"
    * added a button to change the color of the the textbox when pressed

--- WPFDemo/MainWindow.xaml.cs
+++ WPFDemo/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFDemo
 {
@@ -20,9 +9,30 @@ namespace WPFDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Random _random;
+
         public MainWindow()
         {
             InitializeComponent();
+            _random = new Random();
+        }
+
+        private byte[] GetArrayOfRandomBytes(int numberOfBytes)
+        {
+            var randomBytes = new byte[numberOfBytes];
+            _random.NextBytes(randomBytes);
+            return randomBytes;
+        }
+
+        private void SayHelloButton_Click(object sender, RoutedEventArgs e)
+        {
+            var randomBytes = GetArrayOfRandomBytes(3);
+
+            var randomBrushColor = new SolidColorBrush(
+                Color.FromArgb(255, randomBytes[0], randomBytes[1], randomBytes[2]));
+
+            textBox1.BorderBrush = randomBrushColor;
+            textBox1.Foreground = randomBrushColor;
         }
     }
 }
